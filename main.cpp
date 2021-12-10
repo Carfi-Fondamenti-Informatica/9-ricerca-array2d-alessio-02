9c9b1d5ee8a7bdc679061277ce1f02624c24a435
alessio-02 <91601426+alessio-02@users.noreply.github.com>
2021-12-10 23:41:19 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,17 @@
 #include <iostream>
+#include "lib.h"
+using namespace std;
 
-int main(){
-  return 0;
+int main() {
+    char a[10][20], b[1][20];
+    for (int i = 0; i < 10; i++) {
+        cin >> a[i];
+    }
+    cin >> b[0];
+    if (f(a, b)==50){
+        cout << "non presente"<< endl;
+    }else {
+        cout << f(a, b);
+    }
+    return 0;
 }
