771eef86fa6b7e4dd6e9d209298d77dbabb9f45d
alessio-02 <91601426+alessio-02@users.noreply.github.com>
2021-12-11 20:24:56 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,48 +1,12 @@
 #include "lib.h"
+#include <cstring>
 
-void pul (char &a , char &b) {
-    if (a >= 97 and a <= 122 or a >= 65 and a <= 89) {
-
-    } else {
-        a = '#';
-    }
-    if (b >= 97 and b <= 122 or b >= 65 and b <= 89) {
-
-    } else {
-        b = '#';
-    }
-}
-bool controllo(char x, char y){
-    if (x == y) {
-        return true;
-    }else{
-        return false;
-    }
-}
-int f(char a[10][20], char b[1][20]){
-    int X=50;
-
+bool controllo(char a[10][20], char b[1][20], int &X){
     for (int i = 0; i < 10; i++) {
-        for (int j = 0; j < 20; j++) {
-            pul(a[i][j], b[0][j]);
-            if (a[i][j] == '#') {
-                for (int k = j; k < 20; k++) {
-                    a[i][k] = '#';
-                }
-            }
-            if (b[0][j] == '#') {
-                for (int k = j; k < 20; k++) {
-                    b[0][k] = '#';
-                }
-            }
+        if(strcmp(a[i], b[0]) == 0){
+            X=i;
+            return true;
         }
     }
-                for (int i = 0; i < 10; i++) {
-                    for (int j = 0; j < 20; j++) {
-                        if (controllo(a[i][j], b[0][j]) and a[i][j] != '#' and b[0][j] != '#') {
-                            X = i;
-                        }
-                    }
-                }
-        return X;
+    return false;
 }
